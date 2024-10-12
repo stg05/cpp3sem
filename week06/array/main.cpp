b5e9fa260fd140549dc5413f336d977147559841
stg05 <stecenkogeorgii2005@gmail.com>
2024-10-13 02:08:58 +0300

    pow added

--- week06/array/main.cpp
+++ week06/array/main.cpp
@@ -5,99 +5,24 @@
 
 #include <iostream>
 
-template<typename T, unsigned N>
-class Array {
-public:
-    // Список операций:
-    //
-
-    /**  конструктор класса, который создает
-       Array размера size, заполненный значениями
-       value типа T. Считайте что у типа T есть
-       конструктор, который можно вызвать без
-       без параметров, либо он ему не нужен. */
-    explicit Array(const T &t) {
-        m_head = new T[N];
-        m_head[0] = t;
-        m_size++;
-
-    };
-
-    /** Конструктор копирования, который создаёт копию параметра. Считайте, что для типа T определен оператор присваивания. */
-    Array(const Array &arr) {
-        m_head = new T[N];
-        for (unsigned i = 0; i < arr.m_size; i++) {
-            m_head[0] = arr.m_head[i];
-        }
-        m_size = arr.m_size;
+template<typename T, typename P>
+T pow(T param, P pow) {
+    T result = param;
+    if (!std::is_integral_v<P>) {
+        throw std::invalid_argument{"Unsupported type of the 2nd operand"};
     }
-
-    /** деструктор, если он вам необходим. */
-    ~Array() {
-        delete[] m_head;
+    for (P i = 0; i < pow - 1; i++) {
+        result *= param;
     }
+    return result;
+}
 
-    /** оператор копирующего присваивания. */
-    Array &operator=(const Array<T, N> &arr) {
-        if (this == &arr) {
-            return *this;
-        }
-
-        for (unsigned i = 0; i < arr.m_size; i++) {
-            this->m_head[i] = arr.m_head[i];
-        }
-        this->m_size = arr.m_size;
-    };
-
-    /** возвращает размер массива (количество элементов) */
-    [[nodiscard]] std::size_t size() const {
-        return m_size;
-    };
-
-    /** две версии оператора доступа по индексу */
-    T operator[](unsigned idx) {
-        if (idx >= m_size) {
-            throw std::out_of_range("Out of bounds");
-        }
-        return m_head[idx];
-    }
-
-//    T operator[](unsigned) {
-//        return nullptr;
-//    }
-
-    /** получение первого элемента */
-    T front() {
-        return m_head[0];
-    }
-
-    /** получение последнего элемента */
-    T back() {
-        return m_head[m_size - 1];
-    }
-
-    /** проверка пустой ли массив */
-    [[nodiscard]] bool empty() const {
-        return m_size == true;
-    };
-
-    /** заполение массива значением value */
-    void fill(const T &value) const {
-        for (unsigned i = 0; i < N; i++) {
-            m_head[i] = value;
-        }
-    }
-
-private:
-    T *m_head;
-    unsigned m_size = 0;
-};
+template<typename T>
+T pow(T param, char pow) {
+    throw std::invalid_argument{"Unsupported type of the 2nd operand"};
+}
 
 int main() {
-    using namespace std;
-    Array<float, 2> arr{1.5};
-    auto array = arr;
-    cout << array[-1];
-
+    std::cout << pow(1.1, 'a');
     return 0;
 }
