dd8c58214bc2dc0dc4faa719adb8296d803b7e23
stg05 <stg05@outlook.com>
2024-11-17 23:15:19 +0300

    grid mistakes?

--- week06/main.cpp
+++ week06/main.cpp
@@ -1,8 +1,119 @@
 #include <iostream>
 #include <cassert>
+#include <type_traits>
 
 template <typename T, unsigned Dimension>
 class Grid;
+
+template <typename T, unsigned Dimension>
+class Grid
+{
+public:
+    static_assert(Dimension > 0, "Zero-dimensional array");
+    using value_type = T;
+    using size_type = unsigned;
+
+private:
+    Grid<T, Dimension - 1> *m_subgrids;
+    size_type dim_size;
+    // todo allocate
+    void mem_allocate(size_type size)
+    {
+        m_subgrids = static_cast<Grid<T, Dimension - 1> *>(::operator new(sizeof(Grid<T, Dimension - 1>) * size));
+    }
+
+    void mem_free()
+    {
+        delete m_subgrids;
+    }
+
+public:
+    template <typename First, typename... Rest, std::enable_if_t<(sizeof...(Rest) > 0), bool> = true>
+    Grid(const First first, const Rest... rest) : dim_size{(size_type)first}
+    {
+        mem_allocate(dim_size);
+
+        for (size_type i = 0; i < first; i++)
+        {
+            new (m_subgrids + i) Grid<T, Dimension - 1>(rest...);
+        }
+    }
+
+    // Copy constructor
+    Grid(Grid<T, Dimension> const &other)
+    {
+        dim_size = other.dim_size;
+        mem_allocate(dim_size);
+
+        for (size_type i = 0; i < dim_size; i++)
+        {
+            m_subgrids[i] = Grid<T, Dimension - 1>(other.m_subgrids[i]);
+        }
+    }
+
+    // Move constructor
+    Grid(Grid<T, Dimension> &&other)
+    {
+        dim_size = other.dim_size;
+        mem_allocate(dim_size);
+
+        for (size_type i = 0; i < dim_size; i++)
+        {
+            std::swap(m_subgrids[i], other.m_subgrids[i]);
+        }
+    }
+
+    Grid<T, Dimension> &operator=(const Grid<T, Dimension> &rhs)
+    {
+        if (this == &rhs)
+            return *this;
+        // copy and swap
+
+        if (this->dim_size != rhs.dim_size)
+        {
+            this->mem_free();
+            this->dim_size = rhs.dim_size;
+            this->mem_allocate(this->dim_size);
+        }
+        std::copy(rhs.m_subgrids, rhs.m_subgrids + rhs.dim_size, this->m_subgrids);
+        return *this;
+    }
+
+    Grid<T, Dimension> &operator=(Grid<T, Dimension> &&rhs)
+    {
+        if (this == &rhs)
+            return *this;
+        if (this->dim_size != rhs.dim_size)
+        {
+            this->mem_free();
+            this->dim_size = rhs.dim_size;
+            this->mem_allocate(this->dim_size);
+        }
+        std::swap(this->m_subgrids, rhs.m_subgrids);
+        return *this;
+    }
+
+    // Copying operator
+    // non const version
+    const Grid<T, Dimension - 1> &operator[](size_type rhs) const
+    {
+        if ((rhs < 0) || (rhs >= dim_size))
+            throw std::out_of_range("Out of bounds");
+        return m_subgrids[rhs];
+    }
+
+    Grid<T, Dimension - 1> &operator[](size_type rhs)
+    {
+        if ((rhs < 0) || (rhs >= dim_size))
+            throw std::out_of_range("Out of bounds");
+        return m_subgrids[rhs];
+    }
+
+    ~Grid()
+    {
+        mem_free(); // #TODO почему не delete[]? (тогда в valgrind будет mismatch)
+    }
+};
 
 /* Specialization for 1-dim array */
 template <typename T>
@@ -15,9 +126,18 @@ public:
 private:
     T *data;
     size_type dim_size;
+    void mem_allocate(size_type size)
+    {
+        data = new T[size];
+    }
+
+    void mem_free()
+    {
+        delete[] data;
+    }
 
 public:
-    Grid<T, 1>(T &&value)
+    Grid(const T value)
     {
         data = new T[1];
         dim_size = 1;
@@ -26,7 +146,7 @@ public:
 
     Grid(size_type size, T const &fill_value) : dim_size{size}
     {
-        data = new T[size];
+        mem_allocate(size);
         for (size_type i = 0; i < size; i++)
         {
             data[i] = fill_value;
@@ -36,16 +156,23 @@ public:
     // Copy constructor
     Grid(Grid<T, 1> const &other)
     {
-        size_type other_size = other.dim_size;
-        data = new T[other_size];
-        dim_size = other_size;
-        for (size_type i = 0; i < other_size; i++)
+        dim_size = other.dim_size;
+        mem_allocate(dim_size);
+
+        for (size_type i = 0; i < dim_size; i++)
         {
             data[i] = other.data[i];
         }
     }
 
-    const T &operator[](size_type rhs)
+    const T &operator[](size_type rhs) const
+    {
+        if ((rhs < 0) || (rhs >= dim_size))
+            throw std::out_of_range("Out of bounds");
+        return data[rhs];
+    }
+
+    T &operator[](size_type rhs)
     {
         if ((rhs < 0) || (rhs >= dim_size))
             throw std::out_of_range("Out of bounds");
@@ -55,134 +182,22 @@ public:
     ~Grid()
     {
         // std::cout << "trying" << std::endl;
-        delete[] data;
+        mem_free();
         // std::cout << "del data (Dim=1), size = " << dim_size << std::endl;
     }
 };
 
-template <typename T, unsigned Dimension>
-class Grid
-{
-public:
-    static_assert(Dimension > 0, "Zero-dimensional array");
-    using value_type = T;
-    using size_type = unsigned;
-
-private:
-    Grid<T, Dimension - 1> **m_subgrids;
-    size_type dim_size;
-
-public:
-    template <typename First, typename... Rest>
-    Grid(const First first, const Rest... rest) : dim_size{(size_type)first}
-    {
-        m_subgrids = static_cast<Grid<T, Dimension - 1> **>(::operator new(sizeof(Grid<T, Dimension - 1>) * first));
-        for (size_type i = 0; i < first; i++)
-        {
-            m_subgrids[i] = new Grid<T, Dimension - 1>(rest...);
-        }
-    }
-
-    // Copy constructor
-    Grid(Grid<T, Dimension> const &other)
-    {
-        size_type other_size = other.dim_size;
-        dim_size = other_size;
-        m_subgrids = static_cast<Grid<T, Dimension - 1> **>(::operator new(sizeof(Grid<T, Dimension - 1>) * other_size));
-
-        for (size_type i = 0; i < other_size; i++)
-        {
-            m_subgrids[i] = new Grid<T, Dimension - 1>(*(other.m_subgrids[i]));
-        }
-    }
-
-    // Move constructor
-    Grid(Grid<T, Dimension> &&other)
-    {
-        size_type other_size = other.dim_size;
-        dim_size = other_size;
-        m_subgrids = static_cast<Grid<T, Dimension - 1> **>(::operator new(sizeof(Grid<T, Dimension - 1>) * other_size));
-
-        for (size_type i = 0; i < other_size; i++)
-        {
-            m_subgrids[i] = new Grid<T, Dimension - 1>(*(other.m_subgrids[i]));
-        }
-    }
-
-    Grid<T, Dimension> &operator=(Grid<T, Dimension> &rhs)
-    {
-        return rhs;
-    }
-    Grid<T, Dimension> &operator=(Grid<T, Dimension> &&rhs)
-    {
-        return rhs;
-    }
-
-    // Copying operator
-
-    Grid<T, Dimension - 1> &operator[] (size_type rhs) const
-    {
-        if ((rhs < 0) || (rhs >= dim_size))
-            throw std::out_of_range("Out of bounds");
-        return *m_subgrids[rhs];
-    }
-
-    ~Grid()
-    {
-        for (size_type i = 0; i < dim_size; i++)
-        {
-            delete m_subgrids[i];
-        }
-        // std::cout << "Subgrids of dim " << Dimension - 1 << " del'd, proceeding to this of dim "
-        //           << Dimension << " with " << dim_size << " subgrids." << std::endl;
-
-        delete m_subgrids; // #TODO почему не delete[]? (тогда в valgrind будет mismatch)
-    }
-};
-
-// template <typename T>
-// class Grid final
-// {
-// public:
-//     using value_type = T;
-//     using size_type = unsigned;
-
-// private:
-//     T *data;
-//     size_type const m_y_size, m_x_size;
-
-// public:
-//     Grid<T>(size_type y_size, size_type x_size) : m_y_size(y_size), m_x_size(x_size), data(new T[x_size * y_size])
-//     {
-//     }
-
-//     Grid(T const &t)
-//     {
-//         data = new T[1];
-//         data[0] = t;
-//     }
-
-//     // Copy constructor
-//     Grid(Grid<T> const &) = delete;
-
-//     // Move constructor
-//     Grid(Grid<T> &&) = delete;
-
-//     // Copy assginment
-//     Grid<T> &operator=(Grid<T> &) = delete;
-
-//     // Move assignment
-//     Grid<T> &operator=(Grid<T> &&) = delete;
-
 int main()
 {
-    Grid<float, 3> const g3(2, 3, 4, 1.00f);
-    assert(1.0f == g3[1][1][1]);
+    // Grid<float, 3> const g3(10, 10, 10, 1.00f);
+    // assert(1.0f == g3[1][1][1]);
 
     Grid<float, 2> g2(2, 5, 2.0f);
     assert(2.0f == g2[1][1]);
 
-    g2 = g3[1];
-    assert(2.0f == g2[1][1]);
+    // g2 = g3[1];
+    // assert(1.0f == g2[1][1]);
+
+    // auto g1 = Grid<unsigned, 1>(1u);
     return 0;
 }
